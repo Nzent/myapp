07bfde73493706a2946bfb362f31f7ba6cfa4772
nzent <nirvanzentinal@gmail.com>
2022-12-31 18:12:36 +0530

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <>
-    
-      <main >
-     
-      </main>
+      <div>
+        <h1 className="text-3xl font-bold underline text-red-500">Hello worlds!</h1>
+      </div>
     </>
-  )
+  );
 }
